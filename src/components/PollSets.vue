8d5c3fa1d41460959245e583c26ae448853cf235
Arun Gopalakrishnan <arun.gk@ecgroup-intl.com>
2025-11-01 10:06:48 +0530

    Reverted: Pollset updated with icons.

--- src/components/PollSets.vue
+++ src/components/PollSets.vue
@@ -56,36 +56,9 @@
               <div class="text-xs text-neutral mt-1">{{ formatDate(s.createdAt) }}</div>
             </div>
             <div class="flex flex-wrap items-center gap-2 w-full sm:w-auto">
-              <router-link 
-                class="p-2 text-primary hover:bg-primary/10 rounded transition-colors flex-shrink-0" 
-                :to="`/sets/${s.id}/start`"
-                title="Start"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </router-link>
-              <router-link 
-                class="p-2 text-primary hover:bg-primary/10 rounded transition-colors flex-shrink-0" 
-                :to="`/sets/${s.id}/polls`"
-                title="View"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                </svg>
-              </router-link>
-              <button 
-                class="p-2 text-red-600 hover:bg-red-50 rounded transition-colors flex-shrink-0 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent" 
-                @click="() => deleteSet(s.id, s.name)" 
-                :disabled="editingId === s.id"
-                title="Delete"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg>
-              </button>
+              <router-link class="btn text-sm flex-1 sm:flex-none min-w-[80px] justify-center" :to="`/sets/${s.id}/start`">Start</router-link>
+              <router-link class="btn text-sm flex-1 sm:flex-none min-w-[80px] justify-center" :to="`/sets/${s.id}/polls`">View</router-link>
+              <button class="btn bg-red-500 hover:bg-red-600 text-white text-sm flex-1 sm:flex-none min-w-[80px] justify-center" @click="() => deleteSet(s.id, s.name)" :disabled="editingId === s.id">Delete</button>
             </div>
           </div>
         </div>
